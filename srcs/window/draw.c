d183b809a594a7580cccd5ccb1eb81bfd6326295
cyprien <cyprien.augier@protonmail.com>
2021-01-04 11:22:37 +0100

    Fix norm

--- srcs/window/draw.c
+++ srcs/window/draw.c
@@ -6,7 +6,7 @@
 /*   By: caugier <caugier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 18:36:32 by caugier           #+#    #+#             */
-/*   Updated: 2021/01/03 13:41:07 by caugier          ###   ########.fr       */
+/*   Updated: 2021/01/04 11:19:55 by caugier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,37 +67,6 @@ void	init_img_buffer(int width, int height)
 		}
 		g_image_buffer.width = width;
 		black_buffer(height);
-	}
-}
-
-void	black_buffer(int height)
-{
-	t_rgba	color;
-	int	x;
-	int	y;
-	size_t	size;
-	char	*pos;
-
-	color.rgba.red = 0;
-	color.rgba.green = 0;
-	color.rgba.blue = 0;
-	color.rgba.alpha = 255;
-	if (g_mlx != NULL)
-		color.nbr = mlx_get_color_value(g_mlx, color.nbr);
-	size = g_image_buffer.bits_per_pixel / 8;
-	pos = g_image_buffer.address;
-	y = 0;
-	while (y < height)
-	{
-		x = 0;
-		while (x < g_image_buffer.width)
-		{
-			ft_memcpy(pos, &color.nbr, size);
-			pos += size;
-			x++;
-		}
-		y++;
-		pos += g_image_buffer.bytes_per_line;
 	}
 }
 
--- srcs/window/redraw.c
+++ srcs/window/redraw.c
@@ -6,11 +6,48 @@
 /*   By: caugier <caugier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/03 13:36:21 by caugier           #+#    #+#             */
-/*   Updated: 2021/01/03 13:39:58 by caugier          ###   ########.fr       */
+/*   Updated: 2021/01/04 11:20:34 by caugier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "window_int.h"
+#include "../libft/libft.h"
+
+static inline void	get_black(t_rgba *color)
+{
+	color->rgba.red = 0;
+	color->rgba.green = 0;
+	color->rgba.blue = 0;
+	color->rgba.alpha = 255;
+}
+
+void	black_buffer(int height)
+{
+	t_rgba	color;
+	int		x;
+	int		y;
+	size_t	size;
+	char	*pos;
+
+	get_black(&color);
+	if (g_mlx != NULL)
+		color.nbr = mlx_get_color_value(g_mlx, color.nbr);
+	size = g_image_buffer.bits_per_pixel / 8;
+	pos = g_image_buffer.address;
+	y = 0;
+	while (y < height)
+	{
+		x = 0;
+		while (x < g_image_buffer.width)
+		{
+			ft_memcpy(pos, &color.nbr, size);
+			pos += size;
+			x++;
+		}
+		y++;
+		pos += g_image_buffer.bytes_per_line;
+	}
+}
 
 int	redraw(void *param)
 {
